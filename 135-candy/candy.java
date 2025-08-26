15f7806ed7d139ae79e61d5072b47a9e69d640fa
shagun1911 <136734844+shagun1911@users.noreply.github.com>
2025-08-27 00:30:16 +0530

    Time: 2 ms (99.29%) | Memory: 45.8 MB (71.00%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -3,10 +3,6 @@ class Solution {
         int n = ratings.length;
         int[] res = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            res[i] = 1;
-        }
-
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
                 res[i] = res[i - 1] + 1;
@@ -24,6 +20,6 @@ class Solution {
             count += c;
         }
 
-        return count;
+        return count+n;
     }
 }
