fb630fe43b8a1c7fc21aaf728808944c02949fd9
shagun1911 <136734844+shagun1911@users.noreply.github.com>
2025-08-26 22:43:39 +0530

    Time: 2 ms (99.29%) | Memory: 46.9 MB (6.01%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            res[i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                res[i] = res[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                res[i] = Math.max(res[i], res[i + 1] + 1);
+            }
+        }
+
+        int count = 0;
+        for (int c : res) {
+            count += c;
+        }
+
+        return count;
+    }
+}
