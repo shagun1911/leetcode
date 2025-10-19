e066fdb9cfb4d6ecfa2836f1f987b8070c1e69af
shagun1911 <136734844+shagun1911@users.noreply.github.com>
2025-10-19 14:49:02 +0530

    Time: 76 ms (38.20%) | Memory: 46.4 MB (79.78%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.java
@@ -1,37 +1,24 @@
 class Solution {
-
     public String findLexSmallestString(String s, int a, int b) {
-        int n = s.length();
-        String res = s;
-        s = s + s;
-        int g = gcd(b, n);
-        for (int i = 0; i < n; i += g) {
-            for (int j = 0; j < 10; j++) {
-                int kLimit = b % 2 == 0 ? 0 : 9;
-                for (int k = 0; k <= kLimit; k++) {
-                    char[] t = s.substring(i, i + n).toCharArray();
-                    for (int p = 1; p < n; p += 2) {
-                        t[p] = (char) ('0' + ((t[p] - '0' + j * a) % 10));
-                    }
-                    for (int p = 0; p < n; p += 2) {
-                        t[p] = (char) ('0' + ((t[p] - '0' + k * a) % 10));
-                    }
-                    String tStr = new String(t);
-                    if (tStr.compareTo(res) < 0) {
-                        res = tStr;
-                    }
-                }
-            }
-        }
-        return res;
-    }
+        Set<String> vis = new HashSet<>();
+        String smallest = s;
+        Deque<String> q = new ArrayDeque<>();
+        q.offer(s);
+        vis.add(s);
 
-    public int gcd(int num1, int num2) {
-        while (num2 != 0) {
-            int temp = num1;
-            num1 = num2;
-            num2 = temp % num2;
+        while (!q.isEmpty()) {
+            String cur = q.poll();
+            if (cur.compareTo(smallest) < 0) smallest = cur;
+
+            StringBuilder sb = new StringBuilder(cur);
+            for (int i = 1; i < sb.length(); i += 2)
+                sb.setCharAt(i, (char) ((sb.charAt(i) - '0' + a) % 10 + '0'));
+            String added = sb.toString();
+            if (vis.add(added)) q.offer(added);
+
+            String rotated = cur.substring(cur.length() - b) + cur.substring(0, cur.length() - b);
+            if (vis.add(rotated)) q.offer(rotated);
         }
-        return num1;
+        return smallest;
     }
 }
