7a24f68ed0f143385e4a436dd475b0c068dbe237
shagun1911 <136734844+shagun1911@users.noreply.github.com>
2025-11-14 17:56:12 +0530

    Time: 105 ms (6.92%) | Memory: 67.9 MB (16.01%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+     PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int num : nums) {
+            pq.add(num);
+        }
+        
+        for (int i = 0; i < k - 1; i++) {
+            pq.poll();
+        }
+
+        return pq.peek();
+    }
+}
